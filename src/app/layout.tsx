65836f992a89f8948ae3754b342ce0bda7783879
ManishChand349 <manishchandr349@gmail.com>
2025-09-09 18:32:21 +0530

    adding matedata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,9 +13,21 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My News",
-  description: "This Website showing News Only",
+  title: "MyNews | Latest World, Technology, Business & Health News",
+  description: "Stay updated with the latest news from around the world. Explore top stories, technology updates, business trends, health news, and more on MyNews.",
+  keywords: [
+    "news",
+    "world news",
+    "technology news",
+    "business news",
+    "health news",
+    "latest news",
+    "top stories"
+  ],
+  authors: [{ name: "MyNews Team", url: "https://portfoliomanish.vercel.app/" }],
+  viewport: "width=device-width, initial-scale=1.0",
 };
+
 
 export default function RootLayout({
   children,
